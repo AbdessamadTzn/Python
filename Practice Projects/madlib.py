e315bde0706a0dab02a28a9e3bde8c8b047db698
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2023-12-26 12:07:30 +0100

    Add: Mad Lib

---
+++ Practice Projects/madlib.py
@@ -0,0 +1,9 @@
+stduioName = input("Studio's name: ")
+studioMission = input("Studio's mission: ")
+studioSlogan = input("Studio's slogan: ")
+
+
+madlib = f"We are {stduioName}, our goal is to {studioMission}! \
+    Our slogan is {studioSlogan}"
+
+print(madlib)
