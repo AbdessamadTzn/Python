486b32ed36ce081d7e4c8444872b65fc7e745c80
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2023-12-26 12:15:03 +0100

    Add: number guessing

--- Practice Projects/guessNum_Computer.py
+++ Practice Projects/guessNum_Computer.py
@@ -1,3 +1,14 @@
+'''
+Implementation of number gussing by computer or user, by Abdessamad Touzani
+
+YouTube Katsky Studio: https://www.youtube.com/@katskystudio
+Medium Abdessamad Touzani: https://medium.com/@abdessamadtouzani
+Twitter (X) @at9kat: https://twitter.com/at9kat 
+Instagram @t___abdessamad__: https://www.instagram.com/t___abdessamad__/
+Website: https://katskydio.wordpress.com/ or directly links: https://lnk.bio/katskystudio
+Github: https://www.github.com/AbdessamadTzn
+
+'''
 import random
 import time
 
