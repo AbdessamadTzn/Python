5ec216a6080ba6990320a428d8d3585e8db15e10
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2023-12-26 12:15:35 +0100

    Add: Mad Lib

--- Practice Projects/madlib.py
+++ Practice Projects/madlib.py
@@ -1,3 +1,14 @@
+'''
+Implementation of Mad Lib by Abdessamad Touzani
+
+YouTube Katsky Studio: https://www.youtube.com/@katskystudio
+Medium Abdessamad Touzani: https://medium.com/@abdessamadtouzani
+Twitter (X) @at9kat: https://twitter.com/at9kat 
+Instagram @t___abdessamad__: https://www.instagram.com/t___abdessamad__/
+Website: https://katskydio.wordpress.com/ or directly links: https://lnk.bio/katskystudio
+Github: https://www.github.com/AbdessamadTzn
+
+'''
 stduioName = input("Studio's name: ")
 studioMission = input("Studio's mission: ")
 studioSlogan = input("Studio's slogan: ")
