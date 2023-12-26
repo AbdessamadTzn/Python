65937c5129139d3a1a79daae3b22ccccb8f9b422
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2023-12-26 12:13:45 +0100

    Update RockPaperScisor.py

--- Practice Projects/RockPaperScisor.py
+++ Practice Projects/RockPaperScisor.py
@@ -1,3 +1,14 @@
+'''
+Implementation of rock, paper, scissors by Abdessamad Touzani
+
+YouTube Katsky Studio: https://www.youtube.com/@katskystudio
+Medium Abdessamad Touzani: https://medium.com/@abdessamadtouzani
+Twitter (X) @at9kat: https://twitter.com/at9kat 
+Instagram @t___abdessamad__: https://www.instagram.com/t___abdessamad__/
+Website: https://katskydio.wordpress.com/ or directly links: https://lnk.bio/katskystudio
+Github: https://www.github.com/AbdessamadTzn
+
+'''
 import random
 import time
 
